4ed57ee84d38c77e9e150b12de9b0bec7c22656b
Lucas SERTELON <lucas.sertelon@gmail.com>
2026-04-18 23:22:57 +0200

    Added acceleration

--- src/GravityEngine.cpp
+++ src/GravityEngine.cpp
@@ -20,16 +20,25 @@ void calculePosition(Sun &sun, Earth &earth)
     double vecZEarthSun = sun.z - earth.z;
     
     double r = sqrt(vecXEarthSun*vecXEarthSun + vecYEarthSun*vecYEarthSun + vecZEarthSun*vecZEarthSun);
-    
-    double E = (0.5 * (earthVelocity*earthVelocity)) - ((G*sunM) / r);
+    // distance
 
+    double E = (0.5 * (earthVelocity*earthVelocity)) - ((G*sunM) / r);
+    // E <= 0 if hellipse
     if (E > 0){
         return;
     }
     // std::cout << E << std::endl;
 
     double F = (G * sunM * earthM) / (r*r);
-    std::cout << F << std::endl;
+    // std::cout << F << std::endl;
+
+    double forceVecX = F * (vecXEarthSun/r);
+    double forceVecY = F * (vecYEarthSun/r);
+    double forceVecZ = F * (vecZEarthSun/r);
+
+    double accelerationEarthX = forceVecX / earthM; 
+    double accelerationEarthY = forceVecY / earthM; 
+    double accelerationEarthZ = forceVecZ / earthM; 
 
 
 }
